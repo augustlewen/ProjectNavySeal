a2602906beeb7f31328c2a3e6dc9907861d53c3f
augustlewen <augustlewen@live.com>
2020-09-19 17:56:02 +0200

    Added Seal sprite, changes to harpoon script (Harpoon can be thrown and stick to walls)

--- Project Navy Seal/Assets/Scripts/HarpoonController.cs
+++ Project Navy Seal/Assets/Scripts/HarpoonController.cs
@@ -6,24 +6,35 @@ using UnityEngine.UIElements;
 
 public class HarpoonController : MonoBehaviour
 {
-    public bool inHand = true;
-    public bool chargingThrow;
+    public float force;
+    public float maxForce;
+    private bool isInHand = true;
+    private bool isChargingThrow;
+    private bool hasHit;
     private Vector3 startPosition;
+    private Vector2 direction;
+    
+
+    private Rigidbody2D myRigidbody;
+
 
     private void Start()
     {
         startPosition = transform.position;
+
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         Rotate_Harpoon();
         Throw_Harpoon();
+        Airborne();
     }
 
     private void Rotate_Harpoon()
     {
-        if (inHand && chargingThrow == false)
+        if (isInHand && isChargingThrow == false)
         {
             //Get the screen position of the object
             Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
@@ -31,10 +42,8 @@ public class HarpoonController : MonoBehaviour
             //Get the screen position of the mouse
             Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
-
-            float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -angle));
+            direction = mouseOnScreen - positionOnScreen;
+            transform.right = direction;
         }
     }
 
@@ -43,27 +52,39 @@ public class HarpoonController : MonoBehaviour
         //if(chargingThrow == false)
         if (Input.GetButton("Throw"))
         {
-            chargingThrow = true;
-            transform.position = Vector3.Lerp(transform.position, startPosition - transform.right, Time.time * 0.001f);
+            if(force < maxForce)
+            {
+                isChargingThrow = true;
+                force += Mathf.Lerp(0f, maxForce, 0.002f);
+                transform.position = Vector3.Lerp(transform.position, startPosition - transform.right, 0.002f);
+            }
         }
 
         if (Input.GetButtonUp("Throw"))
         {
-            chargingThrow = false;
-            inHand = false;
+            isChargingThrow = false;
+            isInHand = false;
+            
+            myRigidbody.AddForce(transform.right * force);
+            myRigidbody.gravityScale = 0.9f;
         }
-
-
-
-
-        if (chargingThrow == false && transform.position != startPosition)
-            transform.position = Vector3.Lerp(transform.position, startPosition, Time.time * 0.003f);
-
     }
 
-    float AngleBetweenTwoPoints(Vector3 firstPoint, Vector3 secondPoint)
+    private void Airborne()
     {
-        return Mathf.Atan2(firstPoint.y - secondPoint.y, secondPoint.x - firstPoint.x) * Mathf.Rad2Deg;
+        if(isInHand == false && hasHit == false)
+        {
+            direction = myRigidbody.velocity;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        hasHit = true;
+        myRigidbody.velocity = Vector2.zero;
+        myRigidbody.isKinematic = true;
+    }
 }
